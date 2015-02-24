485555bbf5b0cde6992da0fe7f2a689da370f5cf
William Thompson <wthompson13@hawkmail.hccfl.edu>
2015-02-24 15:43:38 -0500

    New file created through use of WindowBuilderPlug-in for Eclipse.

--- src/src/mypkg/Main.java
+++ src/src/mypkg/Main.java
@@ -1,6 +1,12 @@
 // Git collaborative COP-2805 (Java II) project.
 
+//***********
+//This file should not be used. Look at UsingWindowBuilderForSwing.java instead. 
+//***********
 package src.mypkg;
+//package com.example.e4.rcp.todo.ui.parts;
+
+//import javax.annotation.PostConstruct;
 
 /**
  * COP-2805 - Project 3: Creating the architecture of a search engine
@@ -8,8 +14,9 @@ package src.mypkg;
  */
 
 public class Main {
-	
+	//@PostConstruct
 	public static void main(String[] args) {
+	//public void createControls(Composite parent){
 		
         System.out.println("Testing Testing William's Branch"); // Display the string.
         
---
+++ src/src/mypkg/UsingWindowBuilderForSwing.java
@@ -0,0 +1,48 @@
+package src.mypkg;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+
+public class UsingWindowBuilderForSwing extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UsingWindowBuilderForSwing frame = new UsingWindowBuilderForSwing();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UsingWindowBuilderForSwing() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 624, 503);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+	}
+
+}
