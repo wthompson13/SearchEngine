6b5bec74a3deff4a43db5562221159dd9a8e9aec
William Thompson <wthompson13@hawkmail.hccfl.edu>
2015-02-24 15:10:35 -0500

    Creating new java MAIN file for main branch.

---
+++ src/src/mypkg/Main.java
@@ -0,0 +1,19 @@
+// Git collaborative COP-2805 (Java II) project.
+
+package src.mypkg;
+
+/**
+ * COP-2805 - Project 3: Creating the architecture of a search engine
+ * @author William Thompson, David Blocher, Alex Peterson
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+        System.out.println("Testing Testing"); // Display the string.
+        
+    }
+}
+
+    
