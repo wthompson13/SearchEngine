13cbd9ac14de47280ec0b4c1a87cd8dfc724cd9f
William Thompson <wthompson13@hawkmail.hccfl.edu>
2015-02-25 01:12:39 -0500

    Update Main.java
    added comment block for us to talk to each other through the Main.java

--- src/src/mypkg/Main.java
+++ src/src/mypkg/Main.java
@@ -7,6 +7,22 @@ package src.mypkg;
  * @author William Thompson, David Blocher, Alex Peterson
  */
 
+
+/* 
+Use this box to talk to eachother. Delete before sumbmission
+
+1.10 AM Wednesday
+Check out my branch (william) to see a form I made for the main screen. I added a button to open a second form and added a close
+button on that form. For some reason the button text changed when I added an actionListener.
+I thought maybe one of you guys could create the second form for the index information and the other could handle stub methods.
+That is, the methods that we don't have code for yet, like saving the index file and how to import/remove files being searched
+
+
+
+*/
+
+
+
 public class Main {
 	
 	public static void main(String[] args) {
