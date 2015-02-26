9d70d925f5c6bc4fbdffe72b0f41e32a24037fa9
Alman <Alman@Alman-PC.home>
2015-02-26 13:37:23 -0500

    Search Engine GUI.

---
+++ src/src/mypkg/Gui.java
@@ -0,0 +1,144 @@
+
+package src.mypkg;
+
+/**
+ *
+ * @author Alex
+ */
+public class Gui extends java.awt.Frame {
+
+    /**
+     * Creates new form Gui
+     */
+    public Gui() {
+        initComponents();
+    }
+
+    
+ 
+     
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel2 = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton4 = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jCheckBox2 = new javax.swing.JCheckBox();
+        jCheckBox3 = new javax.swing.JCheckBox();
+        jPanel5 = new javax.swing.JPanel();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setTitle("Search Engine 1.0");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                exitForm(evt);
+            }
+        });
+
+        jButton3.setText("About");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton3);
+        jPanel2.add(jProgressBar1);
+
+        add(jPanel2, java.awt.BorderLayout.SOUTH);
+
+        jLabel1.setText("Search For File:");
+        jPanel3.add(jLabel1);
+
+        jTextField1.setText("Search Files");
+        jPanel3.add(jTextField1);
+
+        jButton4.setText("Search");
+        jPanel3.add(jButton4);
+
+        add(jPanel3, java.awt.BorderLayout.NORTH);
+
+        jCheckBox1.setText("Key Word");
+        jPanel4.add(jCheckBox1);
+
+        jCheckBox2.setText("Full File Name");
+        jPanel4.add(jCheckBox2);
+
+        jCheckBox3.setText("Folder");
+        jPanel4.add(jCheckBox3);
+
+        add(jPanel4, java.awt.BorderLayout.EAST);
+
+        jButton2.setText("Remove Selected Files");
+        jPanel5.add(jButton2);
+
+        add(jPanel5, java.awt.BorderLayout.WEST);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.setName("File Table"); // NOI18N
+        jScrollPane1.setViewportView(jTable1);
+
+        add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * Exit the Application
+     */
+    private void exitForm(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_exitForm
+        System.exit(0);
+    }//GEN-LAST:event_exitForm
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Gui().setVisible(true);
+            }
+        });
+    }
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox2;
+    private javax.swing.JCheckBox jCheckBox3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
+// I'm not sure how to change the name of the labels in the table...
