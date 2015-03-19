0a0ee3ec7b03bd41b49bb6e363aa896ac76231c7
Alman <Alman@Alman-PC.home>
2015-03-19 16:24:10 -0400

    Parsing Data take 2

---
+++ Parsing Data/src/parsing/data/ParsingData.java
@@ -0,0 +1,27 @@
+package parsing.data;
+
+/**
+ * Date: 3/19/2015
+ * @author Alex Peterson
+ * I'm not sure on how to parse the data
+ * I looked at the example on the class website, but I'm still confused as to 
+ * the word thing for the txt files. Do we pick any words to search for in the
+ * txt file??
+ */
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class ParsingData {
+
+    
+    public static void main(String[] args) {
+       textFile01 /** For the text files do we pick any word?
+       textFile02
+       textFile03
+       * 
+       */
+    }
+    
+    
+}
