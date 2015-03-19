37b1f6fc7da28f702f4b4485218a06379b4f1db3
Alman <Alman@Alman-PC.home>
2015-03-19 16:54:11 -0400

    Here's a decent Start. (I think)

---
+++ Parsing Files/src/parsing/files/ParsingFiles.java
@@ -0,0 +1,49 @@
+package parsing.data;
+
+/**
+ * Date: 3/19/2015
+ * @author Alex Peterson
+ * I'm not sure on how to parse the data
+ * I looked at the example on the class website, but I'm still confused as to 
+ * the word thing for the txt files. Do we pick any words to search for in the
+ * txt file??
+ */
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class ParsingData {
+public class File
+    
+{
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+      
+    }File file = new File("/path/to/my/file.txt");
+Scanner input = new Scanner((Readable) file);
+
+while
+         /** This was a statement Netbeans threw out. **/
+        private File(String pathtomyfiletxt) {
+            throw new UnsupportedOperationException("Not supported yet."); 
+        }
+input.hasNext()); {
+  {
+  {
+    String nextSpace = input.next();
+    
+    String nextLine = input.nextLine();
+        return null;
+}
+
+input.close();
+return (null);
+    
+    
+}
+
+
