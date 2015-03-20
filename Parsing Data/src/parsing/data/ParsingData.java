8dfb691d0eab22a93ef662ecf02342b506fa9507
Microwhale <Microwhale@users.noreply.github.com>
2015-03-20 19:30:03 -0400

    Response to Alex's question in comments.

--- Parsing Data/src/parsing/data/ParsingData.java
+++ Parsing Data/src/parsing/data/ParsingData.java
@@ -7,6 +7,10 @@ package parsing.data;
  * I looked at the example on the class website, but I'm still confused as to 
  * the word thing for the txt files. Do we pick any words to search for in the
  * txt file??
+ * 
+ * David Blocher: Searching won't be implemented until the next part. What we
+ * need right now is the ability to extract words from the text files to be
+ * used for making the index that we'll later use to do the searching.
  */
 import java.io.IOException;
 import java.nio.file.Path;
